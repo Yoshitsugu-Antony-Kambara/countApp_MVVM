2e7cee911ba85c7071e473a5664648335340aba9
神原 良継 <tsugu1999213@yahoo.co.jp>
2023-03-06 15:24:32 +0900

    WIP

--- countApp_MVVM/countApp_MVVM/Sources/Count/Model/CountModel.swift
+++ countApp_MVVM/countApp_MVVM/Sources/Count/Model/CountModel.swift
@@ -6,7 +6,35 @@
 //
 
 import UIKit
+import RxSwift
+import RxRelay
 
 class CountModel {
 
+    struct Input {
+        let tappedIncrementButton: Observable<Void>
+    }
+
+    struct Output {
+        let incremented: Observable<Int>
+    }
+
+
+    static func transform(input: Input, disposeBag: DisposeBag) -> Output {
+        let numberRelay = BehaviorRelay<Int>(value: 0)
+        var currentNumber: Int = 0
+
+        input.tappedIncrementButton
+            .map { () -> Int in
+                currentNumber = numberRelay.value
+                currentNumber = currentNumber + 1
+                return currentNumber
+            }
+            .bind(to: numberRelay)
+            .disposed(by: disposeBag)
+
+        return Output(
+            incremented: numberRelay.asObservable()
+        )
+    }
 }
